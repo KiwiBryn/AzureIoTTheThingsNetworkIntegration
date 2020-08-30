15554a5edbf5adcfd7d5de2b9619e368f48e1be6
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-08-30 22:55:53 +1200

    Updated to include more field types and better GPS location

--- HttpIntegrationUplink/SeeeduinoLoRaWANCayenneLPP/SeeeduinoLoRaWANCayenneLPP.ino
+++ HttpIntegrationUplink/SeeeduinoLoRaWANCayenneLPP/SeeeduinoLoRaWANCayenneLPP.ino
@@ -47,30 +47,40 @@ void loop(void)
     // https://www.thethingsnetwork.org/docs/devices/arduino/api/cayennelpp.html
     // https://loranow.com/cayennelpp/
     //
-    lpp.addTemperature (1, 27.2);
-    //lpp.addTemperature (2, 25.5);
-    lpp.addLuminosity(1, 100);    
-    lpp.addDigitalInput(1, true);    
-    //lpp.addDigitalInput(2, false);    
-    lpp.addAnalogInput(1, 0.5);
-    lpp.addGPS (1, 4.34, 40.22, 755);
 
     // Core data types all worked
-    //lpp.addDigitalInput(1, true);    
-    //uint8_t addDigitalOutput(uint8_t channel, uint8_t value);
-    //lpp.addAnalogInput(1, 0.5);
-    //uint8_t addAnalogOutput(uint8_t channel, float value);
-    //lpp.addLuminosity(1, 100);    
-    //uint8_t addPresence(uint8_t channel, uint8_t value);
-    //lpp.addTemperature (1, 27.2);
-    //uint8_t addRelativeHumidity(uint8_t channel, float rh);
-    //uint8_t addAccelerometer(uint8_t channel, float x, float y, float z);
-    //uint8_t addBarometricPressure(uint8_t channel, float hpa);
-    //uint8_t addGyrometer(uint8_t channel, float x, float y, float z);
-    //lpp.addGPS (1, 4.34, 40.22, 755)
+    /*
+    lpp.addAnalogInput( 0, 0.01234) ;
+    lpp.addAnalogOutput( 0, 0.56789);
+    lpp.addDigitalInput(0, false);    
+    lpp.addDigitalInput(1, true);    
+    */
+
+    
+    lpp.addGPS (1, -43.5309, 172.6371, 6.192);
+    /*
+    lpp.addAccelerometer(0, 0.0, 0.0, 1.0);
+    lpp.addGyrometer(1, 0.0,0.0,0.0);
+    */
+    
+    /*
+    lpp.addLuminosity(0, 0);    // Pitch black
+    lpp.addLuminosity(1, 8000); // 40w fluro   
+    lpp.addPresence(0, 0);
+    lpp.addPresence(1, 1);
+    */
+    /*
+    lpp.addBarometricPressure(0,0.0);
+    lpp.addBarometricPressure(0,1013.25);
+    lpp.addRelativeHumidity (0, 50.0);
+    lpp.addRelativeHumidity (1, 60.0);
+    lpp.addTemperature (0, -273.00);
+    lpp.addTemperature (1, 0.0);
+    lpp.addTemperature (2, 100.0);
+    */
 
     // Additional data types don't think any of these worked
-    ///lpp.addUnixTime(1, millis()); 
+    //lpp.addUnixTime(1, millis()); 
     //lpp.addGenericSensor(1, 1.23456);
     //lpp.addVoltage(1, 4.5);
     //lpp.addCurrent(0, 1.0);
