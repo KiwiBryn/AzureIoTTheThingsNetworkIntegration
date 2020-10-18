daea1bee3fa2796f60085d371684a92fe42687cd
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-10-18 17:56:25 +1300

    TTN Client using MQTTNet

--- TTNMQTTIntegrationClient/Program.cs
+++ TTNMQTTIntegrationClient/Program.cs
@@ -17,6 +17,7 @@
 namespace devMobile.TheThingsNetwork.TTNMQTTIntegrationClient
 {
    using System;
+   using System.Collections.Generic;
    using System.Diagnostics;
    using System.Threading.Tasks;
 
@@ -25,6 +26,8 @@ namespace devMobile.TheThingsNetwork.TTNMQTTIntegrationClient
    using MQTTnet.Client.Disconnecting;
    using MQTTnet.Client.Options;
    using MQTTnet.Client.Receiving;
+
+   using Newtonsoft.Json;
 
    class Program
    {
@@ -113,11 +116,13 @@ namespace devMobile.TheThingsNetwork.TTNMQTTIntegrationClient
       {
          if (e.ApplicationMessage.Topic.EndsWith("/up"))
          {
-            Console.WriteLine($"ClientId:{e.ClientId} Topic:{e.ApplicationMessage.Topic} Payload:{e.ApplicationMessage.ConvertPayloadToString()}");
+            PayloadUplink payload = JsonConvert.DeserializeObject<PayloadUplink>(e.ApplicationMessage.ConvertPayloadToString());
+
+            Console.WriteLine($"{DateTime.UtcNow:HH:MM:ss} ClientId:{e.ClientId} Topic:{e.ApplicationMessage.Topic} ReceivedAt:{payload.Metadata.ReceivedAtUtc} Payload:{payload.PayloadRaw}");
          }
          else
          {
-            Console.WriteLine($"ClientId:{e.ClientId} Topic:{e.ApplicationMessage.Topic}");
+            Console.WriteLine($"{DateTime.UtcNow:HH:MM:ss} ClientId:{e.ClientId} Topic:{e.ApplicationMessage.Topic}");
          }
       }
 
@@ -135,5 +140,71 @@ namespace devMobile.TheThingsNetwork.TTNMQTTIntegrationClient
             Debug.WriteLine("Reconnect failed {0}", ex.Message);
          }
       }
+   }
+
+   // Production version of classes for unpacking HTTP payload https://json2csharp.com/
+   public class Gateway // https://github.com/TheThingsNetwork/ttn/blob/36761935d1867ce2cd70a80ceef197a124e2d276/core/types/gateway_metadata.go
+   {
+      [JsonProperty("gtw_id")]
+      public string GatewayId { get; set; }
+      [JsonProperty("timestamp")]
+      public ulong Timestamp { get; set; }
+      [JsonProperty("time")]
+      public DateTime ReceivedAtUtc { get; set; }
+      [JsonProperty("channel")]
+      public int Channel { get; set; }
+      [JsonProperty("rssi")]
+      public int Rssi { get; set; }
+      [JsonProperty("snr")]
+      public double Snr { get; set; }
+      [JsonProperty("rf_chain")]
+      public int RFChain { get; set; }
+      [JsonProperty("latitude")]
+      public double Latitude { get; set; }
+      [JsonProperty("longitude")]
+      public double Longitude { get; set; }
+      [JsonProperty("altitude")]
+      public int Altitude { get; set; }
+   }
+
+   public class Metadata
+   {
+      [JsonProperty("time")]
+      public DateTime ReceivedAtUtc { get; set; }
+      [JsonProperty("frequency")]
+      public double Frequency { get; set; }
+      [JsonProperty("modulation")]
+      public string Modulation { get; set; }
+      [JsonProperty("data_rate")]
+      public string DataRate { get; set; }
+      [JsonProperty("coding_rate")]
+      public string CodingRate { get; set; }
+      [JsonProperty("gateways")]
+      public List<Gateway> Gateways { get; set; }
+   }
+
+   public class PayloadUplink
+   {
+      [JsonProperty("app_id")]
+      public string ApplicationId { get; set; }
+      [JsonProperty("dev_id")]
+      public string DeviceId { get; set; }
+      [JsonProperty("hardware_serial")]
+      public string DeviceEui { get; set; }
+      [JsonProperty("port")]
+      public int Port { get; set; }
+      [JsonProperty("counter")]
+      public int Counter { get; set; }
+      [JsonProperty("is_retry")]
+      public bool IsRetry { get; set; }
+      [JsonProperty("Payload_raw")]
+      public string PayloadRaw { get; set; }
+      // finally settled on an Object
+      [JsonProperty("payload_fields")]
+      public Object PayloadFields { get; set; }
+      [JsonProperty("metadata")]
+      public Metadata Metadata { get; set; }
+      [JsonProperty("downlink_url")]
+      public string DownlinkUrl { get; set; }
    }
 }
